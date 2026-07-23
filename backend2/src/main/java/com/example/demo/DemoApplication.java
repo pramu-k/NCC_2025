d0b48faa82663b3e8298215848e119c69abd9934
pramu-k <pramukaluthanthri@gmail.com>
2026-07-23 20:19:47 +0530

    added java project changes

--- backend2/src/main/java/com/example/demo/DemoApplication.java
+++ backend2/src/main/java/com/example/demo/DemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api")
 public class DemoApplication {
